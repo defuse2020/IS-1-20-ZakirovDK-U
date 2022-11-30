aaec6d36367b177e32fa40a5b1d4cf6fbca23877
z36123 <z36123@206-10>
2022-11-30 17:19:24 +0500

    Добавьте файлы проекта.

---
+++ IS-1-20-ZakirovDK-U/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_1_20_ZakirovDK_U
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zadanie1 Z1 = new Zadanie1();
+            Z1.ShowDialog();
+        }
+    }
+}
---
+++ IS-1-20-ZakirovDK-U/Zadanie1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_1_20_ZakirovDK_U
+{
+    public partial class Zadanie1 : Form
+    {
+        class Complect
+        {
+            int price;
+            int sience;
+            public Complect(int price,int sience)
+            {
+                this.price = price;
+                this.sience = sience;
+            }
+            public void Display()
+            {
+
+            }
+        }
+        public Zadanie1()
+        {
+            InitializeComponent();
+        }
+    }
+}
