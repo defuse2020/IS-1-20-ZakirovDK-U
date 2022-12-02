c936feaedf70cbfcc16c85d42e0802bfdbc506b2
z36123 <z36123@outlook.com>
2022-12-02 17:07:37 +0500

    Задание 2 (добавление комментариев)

--- IS-1-20-ZakirovDK-U/Zadanie2.cs
+++ IS-1-20-ZakirovDK-U/Zadanie2.cs
@@ -13,14 +13,15 @@ namespace IS_1_20_ZakirovDK_U
 {
     public partial class Zadanie2 : Form
     {
+        //создаём класс
         public class Connection
         {
+            //создаём метод MySqlConnection, который будет выполнять подключение формы к БД
             public MySqlConnection Conn()
             {
-                string connect = "server=chuc.caseum.ru;port=33333;user=uchebka;database=uschebka;password=uchebka;";
-                MySqlConnection conn;
-                conn = new MySqlConnection(connect);
-                return conn;
+                string connect = "server=chuc.caseum.ru;port=33333;user=uchebka;database=uschebka;password=uchebka;"; //переменная string, содержащая строку подключения к БД
+                MySqlConnection conn = new MySqlConnection(connect); ; //экземпляр класса MySqlConnection, в который пишем переменную строки подключения
+                return conn; //возвтрат строки подключения
             }
         }
         public Zadanie2()
@@ -30,13 +31,17 @@ namespace IS_1_20_ZakirovDK_U
 
         private void Zadanie2_Load(object sender, EventArgs e)
         {
+            //создаём экземпляр класса Connection
             Connection Conn1 = new Connection();
+            //прогоняем выполнение метода через конструкцию try..catch
             try
             {
+                //выполнение метода
                 Conn1.Conn();
             }
             catch
             {
+                //если возникнет исключение, вылетит сообщение об ошибке
                 MessageBox.Show("Возникло исключение");
             }
         }
